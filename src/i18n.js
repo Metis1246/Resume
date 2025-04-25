f59615c1996d7fe075f0abaf19afb13a2312c05a
Metis_12 <metis0987568205@gmail.com>
2025-04-26 01:17:46 +0700

    th

---
+++ src/i18n.js
@@ -0,0 +1,22 @@
+export const messages = {
+  en: {
+    home: "Home",
+    about: "About Me",
+    skills: "Skills",
+    experience: "Experience",
+    projects: "Projects",
+    contact: "Contact",
+    hello: "Hello",
+    metis: "Metis Namaung",
+  },
+  th: {
+    home: "หน้าหลัก",
+    about: "เกี่ยวกับฉัน",
+    skills: "ความสามารถ",
+    experience: "ประสบการณ์",
+    projects: "โปรเจกต์",
+    contact: "ติดต่อ",
+    hello: "สวัสดี",
+    metis: "เมธีส นาเหมือง",
+  },
+};
