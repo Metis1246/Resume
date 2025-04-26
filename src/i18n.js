117629dddbecacca91f2ca6935e6af45e13459e5
Xcasterz <xct.xcaster@gmail.com>
2025-04-26 16:05:50 +0700

    test

--- src/i18n.js
+++ src/i18n.js
@@ -2,8 +2,6 @@ import { createI18n } from "vue-i18n";
 import th from "./i18n/th";
 import en from "./i18n/en";
 
-// บันทึกการตั้งค่าภาษาในท้องถิ่น
-
 const getSavedLocale = () => {
   // ตรวจสอบว่าอยู่ใน Browser ก่อนใช้ localStorage
   if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
@@ -19,9 +17,5 @@ const i18n = createI18n({
   silentTranslationWarn: true, // ปิด warning
   messages: { TH: th, EN: en }
 });
-
-const { locale, availableLocales } = useI18n();
-console.log("Current locale:", locale.value);
-console.log("Available locales:", availableLocales);
 
 export default i18n;
