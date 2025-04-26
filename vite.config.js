4180492a51d618ae73a361553fcf10133b7a5c95
Metis_12 <metis0987568205@gmail.com>
2025-04-26 12:04:44 +0700

    detail

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Pages from "vite-plugin-pages";
 import path from "path";
-import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 
 export default defineConfig({
   plugins: [
@@ -11,17 +10,10 @@ export default defineConfig({
       dirs: "src/views",
       extensions: ["vue"],
     }),
-    VueI18nPlugin({
-      include: path.resolve(__dirname, "./src/i18n/**"), // ระบุ path ไปยังไฟล์ภาษา
-      runtimeOnly: false,
-    }),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "src"), // กำหนด alias @ ให้ชี้ไปที่โฟลเดอร์ src
     },
-  },
-  build: {
-    chunkSizeWarningLimit: 1600, // เพิ่มขนาด chunk สูงสุด
   },
 });
